830eac780c902da85e5318fcbc42372846585e1c
ljsalcedo-dev <ljsalcedo.dev@gmail.com>
2026-06-25 22:50:25 +0800

    feat: add VITE_COMING_SOON env var type to ImportMetaEnv

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,22 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_COMING_SOON: string;
+  readonly VITE_GOVERNMENT_NAME: string;
+  readonly VITE_GOVERNMENT_TYPE: string;
+  readonly VITE_REGION: string;
+  readonly VITE_PROVINCE: string;
+  readonly VITE_WEBSITE_URL: string;
+  readonly VITE_CONTACT_EMAIL: string;
+  readonly VITE_CONTACT_PHONE: string;
+  readonly VITE_FACEBOOK_URL: string;
+  readonly VITE_TWITTER_URL: string;
+  readonly VITE_INSTAGRAM_URL: string;
+  readonly VITE_YOUTUBE_URL: string;
+  readonly VITE_SITE_DESCRIPTION: string;
+  readonly VITE_SITE_KEYWORDS: string;
+  readonly VITE_OG_IMAGE_URL: string;
+  readonly VITE_TWITTER_HANDLE: string;
+  readonly VITE_GOOGLE_ANALYTICS_ID: string;
+  readonly VITE_FACEBOOK_PIXEL_ID: string;
+}
